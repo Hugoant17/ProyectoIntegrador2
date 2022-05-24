a1799a3c62027926a4932d0ef3b589192fcb629c
Hugo A. Zevallos <hugoanthonio17@gmail.com>
2022-05-24 00:52:13 -0500

    modificacion 2

--- app/src/main/java/com/example/proyectointegrador2/MenuAdmin.java
+++ app/src/main/java/com/example/proyectointegrador2/MenuAdmin.java
@@ -2,13 +2,33 @@ package com.example.proyectointegrador2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuAdmin extends AppCompatActivity {
+
+    private Button mButtonSignOut;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_admin);
+
+        mAuth = FirebaseAuth.getInstance();
+        mButtonSignOut = (Button) findViewById(R.id.btnCerrarSes);
+
+        mButtonSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(MenuAdmin.this,MainActivity.class));
+                finish();
+            }
+        });
     }
 }
